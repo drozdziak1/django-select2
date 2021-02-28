038a763d9caab00be2d583675edf8a87618af5c7
Stan Drozd <drozdziak1@gmail.com>
2021-02-28 23:54:24 +0100

    WIP: requirements overhaul

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 from setuptools import setup
 
+import os
+
+# NOTE(drozdziak1): Hardcoded for PBR
+os.environ["PBR_VERSION"] = "6.3.1-pbr-devdep"
+
 setup(
     setup_requires=['pbr'],
     pbr=True,
